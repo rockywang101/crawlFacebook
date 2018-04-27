15c1d72f1a6c3d277f4cdee69bdb29c3e8447680
rocky.wang <rockywang101@gmail.com>
2018-04-27 12:18:28 +0800

    1

--- crawlFacebookToGooglesheet.py
+++ crawlFacebookToGooglesheet.py
@@ -4,33 +4,36 @@ Demo fetch Facebook FanPage data
 Created on 2018年3月19日
 @author: rocky.wang
 '''
+import time, os, json, requests
 from googleService import GooglesheetService
-import time
-
-'''
-Created on 2018年3月13日
-@author: rocky.wang
-'''
-import json, os, requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+import datetime
 
 sheetService = GooglesheetService("1bydUUYVTxyyz2jxm3JRzIQkdElNthLHuQpzW6-DLVUk")
 
 
 def main():
+    print("執行時間 {}".format(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
+    
+    token = fetchToken()
     
     rowList = sheetService.getValues("粉絲頁列表")
     for row in rowList:
         fanPageId = row[1]
         rangeName = row[2]
-        fetchFanPage(fanPageId, rangeName)
+        fetchFanPage(fanPageId, rangeName, token)
         time.sleep(1) # 不睡似乎也不會怎麼樣，但反正一小時一次而已，睡一下比較安全
 
-def fetchFanPage(fanPageId, rangeName):
+
+def fetchFanPage(fanPageId, rangeName, token):
     
     # 取已經寫入過的 post id
     existPostIds = getExistPostIds(rangeName)
     # 取前 25 筆貼文
-    rowList = crawlFanpageData(fanPageId)
+    rowList = crawlFanpageData(fanPageId, token)
     # 過濾剩下沒寫入過的貼文
     rowList = [row for row in rowList if not row[2] in existPostIds]
     
@@ -47,11 +50,10 @@ def fetchFanPage(fanPageId, rangeName):
 permalink_url 為粉絲頁文章原始連結
 link 為文章張貼的連結 (因為後來才加上去，放在最後面)
 '''
-def crawlFanpageData(fanPageId):
+def crawlFanpageData(fanPageId, token):
     
-    print(os.environ["FACEBOOK_ACCESS_TOKEN"])
-    
-    token = "EAACEdEose0cBAIk2ifpuVkJlt5D7yxxbh61H0mCgZBZAZA3FVb9J0inwlZAg1xvZBuBBBC9RLXYSEGqBYZC7LHHeZA4HrhEkeAjU6Pd93pjhhxZCfqrJCaWBdJNm7rlpP1znflDsSdJJVou86EGyITnn1a0nqC8BWkxF43ZA10n4teCFaLMrIFsphimsii9ZCRZB2lDVENAkYG7oQZDZD"
+#     print(os.environ["FACEBOOK_ACCESS_TOKEN"])
+#     token = "EAACEdEose0cBAOBGwisffpqJznCSvW2qzLHruyTKYyrZB91NTmVVsUx0ALau7qcdqAxMPWLkCQg4bHFz2ZAONb3QwlaLl6BHwHjKSuvsiIWxOZAQ0JUQb8lis1IpO5fUb6IDoMQODPIiGCiGrjGOVkP9h3i96l8ScgHT5ZBZCf07igFpMcIibGqeTWysuTBfzkGqIvS8BdwZDZD"
     
     fields = "id, name, posts{id,name,message,created_time,link,permalink_url}"
     url = 'https://graph.facebook.com/v2.10/{}?fields={}&access_token={}'.format(fanPageId, fields, token)
@@ -76,6 +78,49 @@ def getExistPostIds(rangeName):
     for row in sheetRows:
         existPostIds.append(row[2])
     return existPostIds
+
+
+def fetchToken():
+        
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(firefox_options=options)
+    
+    print("開啟臉書")
+    driver.get("https://www.facebook.com/")
+    time.sleep(5)
+    print("進行登入")
+    elem = driver.find_element_by_id("email")
+    elem.clear()
+    elem.send_keys("rockywang101@gmail.com")
+    elem = driver.find_element_by_id("pass")
+    elem.clear()
+    elem.send_keys(os.environ["MY_PASSWORD"])
+    elem.send_keys(Keys.RETURN)
+    time.sleep(5)
+    
+    print("進入 API 頁面")
+    driver.get("https://developers.facebook.com/tools/explorer")
+    time.sleep(5)
+    print("取得權杖")
+    elem = driver.find_element_by_link_text("取得權杖")
+    elem.click()
+    time.sleep(2)
+    print("取得用戶存取權杖")
+    elem2 = driver.find_element_by_link_text("取得用戶存取權杖")
+    elem2.click()
+    time.sleep(2)
+    print("取得存取權杖")
+    elem3 = driver.find_element(By.XPATH, '//button[text()="取得存取權杖"]')
+    elem3.click()
+    time.sleep(5)
+    
+    inputList = driver.find_elements_by_class_name("_58al")
+    token = inputList[2].get_attribute('value')
+    print("取得 API Token: " + token)
+    
+    driver.close()
+    return token
 
 
 
